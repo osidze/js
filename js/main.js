70a51b8089513c009a9b17474149ceb512c500d8
Olga <osidze12@gmail.com>
2018-11-23 00:59:06 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+var  myText = 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+     myNumber = 25,   
+     myBool = true;
+    console.log(myText);
+    console.log(myNumber);
+    console.log(myBool);
+
+var obj = {name: "Lorem ipsum dolor sit amet."},
+    array = [1, 2, 3];
+    console.log(obj);
+    console.log(array);
+
+var n = 10;
+    console.log(n.toFixed(2));
+
+var i = 15;
+    console.log(++i);
+    console.log(i);
+    console.log(i++);
+    console.log(i);
+
+var a = 20;
+    a += 20;
+    console.log(a);
+
+var a = 20;
+    a -= 3;
+    console.log(a);
+
+var a = 20;
+    a *= 3;
+    console.log(a);
+
+var a = 20;
+    a /= 5;
+    console.log(a);
+
+var a = 20;
+    a %= 4;
+    console.log(a);
+
+console.log(Math.PI);
+
+console.log(Math.round(89.279));
+
+console.log(Math.random()*10);
+
+console.log(Math.pow(2,5));
+
+console.log(myText.length);
+
+console.log(myText.indexOf('ips'));
+console.log(myText.indexOf('blablabla'));
+
+console.log(myText.replace('dolor', 'replacethisword'));
+console.log(myText);
+
+console.log(myText.toUpperCase());
+console.log(myText.toLowerCase());
